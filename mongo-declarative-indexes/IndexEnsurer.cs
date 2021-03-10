ab7d03b2fd22e3080d9c29127de0e9ba7e51618a
askazakov <askazakov@gmail.com>
2021-03-10 17:04:24 +0500

    initial commit

---
+++ mongo-declarative-indexes/IndexEnsurer.cs
@@ -0,0 +1,19 @@
+﻿namespace MongoDB.DeclarativeIndexes
+{
+    public class IndexEnsurer
+    {
+        public void Ensure()
+        {
+            Begin();
+            End();
+        }
+        
+        public void Begin()
+        {
+        }
+
+        public void End()
+        {
+        }
+    }
+}
